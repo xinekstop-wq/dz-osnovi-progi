533fdf616f6ccc8f76d871295e2c0631e6dad785
xinekstop-wq <xinekstop@gmail.com>
2026-09-14 23:04:48 +0300

    лаба дз 1

---
+++ дз к лабораторной работе №1/lab1dz/Source.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	puts("          /\\         ");
+	puts("         /  \\         ");
+	puts("        /    \\        ");
+	puts("       /      \\         ");
+	puts("      /________\\          ");
+	puts("      | [] []  |   ");
+	puts("      |  [] [] |   ");
+	puts("      |________|   ");
+	return 0;
+}
